fc723ac9ffaaf6a16df186b31923957ac27ce34b
christo pb <christo@ateamsoftsolutions.com>
2024-06-29 01:47:31 -0400

    Update: removed print

--- WebApplication1/Models/common/ResponseModel.cs
+++ WebApplication1/Models/common/ResponseModel.cs
@@ -7,24 +7,21 @@ public class ApiResponse<T> : ObjectResult
     public ApiResponse(DataBaseResult<T> result) : base(result)
     {
         StatusCode = result.StatusCode;
-        Console.Write($"Writing status code {result.StatusCode}");
-}
+    }
     
-    
-         public static IActionResult GenerateErrorResponseModel(ActionContext context)
-     {
-         List<string> apiError = [];
-         var errorList = context.ModelState.AsEnumerable();
+    public static IActionResult GenerateErrorResponseModel(ActionContext context)
+    {
+        List<string> apiError = [];
+        var errorList = context.ModelState.AsEnumerable();
 
-         apiError.AddRange(from e in errorList from ie in e.Value!.Errors select ie.ErrorMessage);
+        apiError.AddRange(from e in errorList from ie in e.Value!.Errors select ie.ErrorMessage);
 
-         return new  ApiResponse<T?>(new DataBaseResult<T?>(
-             statusCode:400,
-             data:default,
-             errors: apiError,
-             message:"Bad request",
-             success:false
-             ));
-     }
-    
+        return new ApiResponse<T?>(new DataBaseResult<T?>(
+            statusCode: 400,
+            data: default,
+            errors: apiError,
+            message: "Bad request",
+            success: false
+        ));
+    }
 }
