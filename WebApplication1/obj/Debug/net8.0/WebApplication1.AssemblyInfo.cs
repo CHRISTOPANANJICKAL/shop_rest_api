0a7fd5aea40a4318e71fe9dacc50433e9174b13e
christo pb <christo@ateamsoftsolutions.com>
2024-06-29 01:46:31 -0400

    Ignore: Autogenerated

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9cf53f77a1cb64c2e67285e97eaa8ccce99baeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f8bb9169c3bb27afc30085884616d0364c001d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
